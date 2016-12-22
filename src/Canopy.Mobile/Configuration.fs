acf7a2742fb2a9b91f92b59b34df7d62b740e43e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-12-22 17:03:04 +0100

    Timing

--- src/Canopy.Mobile/Configuration.fs
+++ src/Canopy.Mobile/Configuration.fs
@@ -3,7 +3,7 @@
 open System.IO
 
 /// TimeOut for find operations
-let mutable waitTimeout = 10.0
+let mutable waitTimeout = 20.0
 
 /// Android Emulator tool name
 let mutable androidEmulatorToolName = "emulator.exe"
