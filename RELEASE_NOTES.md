5add391b1bddf20079676e1bfd21d96f32534d86
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-01-03 11:57:51 +0100

    Allow menu-tv convention

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
-### 1.1.4 - 29.12.2016
+### 1.1.5 - 03.01.2017
 * Starts Android emulator directly from Canopy.Mobile
 * Start appium directly from Canopy.Mobile
 * Allow to take screenshot
--- src/Canopy.Mobile/Selectors.fs
+++ src/Canopy.Mobile/Selectors.fs
@@ -1,16 +1,17 @@
 ﻿[<AutoOpen>]
 module canopy.mobile.selectors
 
-/// Selector Conventions that determine if a selector matches a pattern and if it does what the resulting By is
+/// Selector conventions that determine if a selector matches a pattern and if it does what the resulting By is
 let mutable selectorConventions =
     [
         fun (selector : string) -> if selector.StartsWith("#") then Some (OpenQA.Selenium.By.Id(selector.Replace("#", "android:id/"))) else None
         fun (selector : string) -> if selector.StartsWith("tv:") then Some (OpenQA.Selenium.By.XPath(sprintf """//android.widget.TextView[@text="%s"]""" (selector.Replace("tv:", "")))) else None
+        fun (selector : string) -> if selector.StartsWith("menu-tv:") then Some (OpenQA.Selenium.By.XPath(sprintf """//android.widget.TextView[@content-desc="%s"]""" (selector.Replace("menu-tv:", "")))) else None
         fun (selector : string) -> if selector.StartsWith("android:id") then Some (OpenQA.Selenium.By.Id(selector)) else None
         fun (selector : string) -> if selector.StartsWith("//") then Some (OpenQA.Selenium.By.XPath(selector)) else None
     ]
 
-/// The default Selector Convetion if none of the configurable Selector Convetions are matched
+/// The default Selector convention if none of the configurable Selector conventions are matched
 let mutable defaultSelectorConvention = fun selector -> OpenQA.Selenium.By.XPath(sprintf """//*[@text="%s"]""" selector)
 
 /// Using conventions convert a string representing a selector to a Selenium By
